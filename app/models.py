36b0e7973f65f23e9c4935ccb768c451a7b4fc01
otavius <developer.otavius@gmail.com>
2025-05-24 08:44:10 -0400

    Chore: Djano authentication and soical sign-on

--- app/models.py
+++ app/models.py
@@ -1,20 +1,33 @@
 import re
 from django.db import models
+from django.utils.translation import gettext_lazy as _ 
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+
+from app.managers import UserProfileManager
 
 ARTICLE_STATUS = (("draft", "draft"), ("inprogress", "inprogress"), ("published", "published"))
 
 class UserProfile(AbstractUser):
-    pass 
+    email = models.EmailField(_("email address"), max_length=255, unique=True)
+    objects = UserProfileManager()
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
 
 class Article(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField(blank=True, default="")
-    word_count = models.IntegerField(blank=True, default="")
-    twitter_post = models.TextField(blank=True, default="")
-    status = models.CharField(max_length=20, choices=ARTICLE_STATUS, default="draft")
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta: 
+        verbose_name = _("Article")
+        verbose_name_plural = _("Articles")
+    title = models.CharField(_("Title"),max_length=100)
+    content = models.TextField(_("Content"),blank=True, default="")
+    word_count = models.IntegerField(_("Word Count"),blank=True, default="")
+    twitter_post = models.TextField(_("X Post"),blank=True, default="")
+    status = models.CharField(_("Status"),max_length=20, choices=ARTICLE_STATUS, default="draft")
+    created_at = models.DateTimeField(_("Created at"),auto_now_add=True)
+    updated_at = models.DateTimeField(_("Updated at"),auto_now=True)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL,verbose_name=_("Creator"), on_delete=models.CASCADE, related_name="articles")
 
     # calculates the word count for a article 
     def save(self, *args, **kwargs):
--- app/views.py
+++ app/views.py
@@ -3,35 +3,50 @@ from django.urls import reverse_lazy
 from app.forms import CreateArticleForm
 from app.models import Article
 from django.views.generic import CreateView, ListView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Class Based views 
 # def home(request):
 #     articles = Article.objects.all()
 #     return render(request, "app/home.html", {"articles": articles})
-class ArticleListView(ListView):
+
+class ArticleListView(LoginRequiredMixin, ListView):
     template_name = "app/home.html"
     model = Article
     context_object_name = "articles"
 
-class ArticleCreateView(CreateView):
+    def get_queryset(self):
+        return Article.objects.filter(creator=self.request.user).order_by("-created_at")
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     template_name = "app/article_create.html"
     model = Article
     fields = ["title", "status", "content", "twitter_post"]
     success_url = reverse_lazy("home")
 
-class ArticleUpdateView(UpdateView):
+    def form_valid(self, form):
+        form.instance.creator = self.request.user
+        return super().form_valid(form)
+
+class ArticleUpdateView(LoginRequiredMixin,UserPassesTestMixin, UpdateView):
     template_name = "app/article_update.html"
     model = Article
     fields = ["title", "status", "content", "twitter_post"]
     success_url = reverse_lazy("home")
     context_object_name = "article"
 
-class ArticleDeleteView(DeleteView):
+    def test_func(self):
+        return self.request.user == self.get_object().creator
+
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     template_name = "app/article_delete.html"
     model = Article
     success_url = reverse_lazy("home")
     context_object_name = "article"
     contecxt_object_name = "article"
+
+    def test_func(self):
+        return self.request.user == self.get_object().creator
 
     
 # function based views
